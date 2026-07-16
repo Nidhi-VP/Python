2cfbb59fd7bf313da77d30fab13b27304f13fc2d
Nidhi-VP <nidhipoojari2207@gmail.com>
2026-07-16 18:35:04 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,14 @@
+name = "John"
+age=20
+height=5.8
+is_student=True
+email=None
+siblings=["sara","lisa"]
+address={"street":"123 main st","city":"California"}
+
+print("name:",name)
+print("age:",age)
+print("is student:",is_student)
+print("email:",email)
+print("siblings:",siblings)
+print("address:",address)
