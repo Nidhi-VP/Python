529e7103109f2bf7a16e21de8cfde6da92b80f50
Nidhi-VP <nidhipoojari2207@gmail.com>
2026-07-20 11:59:44 +0530

    day4.py

---
+++ day4.py
@@ -0,0 +1,30 @@
+num1=10
+num2=3
+
+print("\n---Arithmetic operations---")
+print("addition(10+3):",num1+num2)
+print("subtraction(10-3):",num1-num2)
+print("multiplication(10*3):",num1*num2)
+print("division(10/3):",num1/num2)
+print("floor division(10//3):",num1//num2)
+print("modulus(10%3):",num1%num2)
+print("exponentiation(10**3):",num1**num2)
+
+print("\n--Relational operations--")
+print("is 10 equal to 37",num1==num2)
+print("is 10 not equal to 37",num1!=num2)
+print("is 10 greater than 37",num1>num2)
+print("is 10 less than 37",num1<num2)
+
+true_value=True
+false_value=False
+
+print("\n--Logical operations--")
+print("true AND false",true_value and false_value)
+print("true AND true",true_value and false_value)
+print("false AND false",true_value and false_value)
+print("true or false",true_value and false_value)
+print("true or true",true_value and false_value)
+print("false or false",true_value and false_value)
+print("not true",true_value and false_value)
+print("not false",true_value and false_value)
