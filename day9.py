3db3eb3e1b34e2fc98c1917b1da4b299d3389640
Nidhi-VP <nidhipoojari2207@gmail.com>
2026-08-05 23:44:57 +0530

    Random number generator

---
+++ day9.py
@@ -0,0 +1,16 @@
+1. Write a program that generates a random number.
+
+import random
+
+# Generate a random floating-point number between 0 and 1
+num = random.random()
+print("Random number:", num)
+
+
+2. Write a program that generates random number between 2 integers
+
+import random
+# Random float between 5 and 10
+num = random.uniform(5, 10)
+print("Random float between 5 and 10:", num)
+
