79f8f27638394ac8730cceeec474e543e6917b2a
Nidhi-VP <nidhipoojari2207@gmail.com>
2026-08-05 23:49:02 +0530

    random number generation
    Added functionality to generate random numbers between specified ranges.

--- day9.py
+++ day9.py
@@ -5,6 +5,7 @@ import random
 # Generate a random floating-point number between 0 and 1
 num = random.random()
 print("Random number:", num)
+o/p:Random number: 0.44010289148341175
 
 
 2. Write a program that generates random number between 2 integers
@@ -13,4 +14,6 @@ import random
 # Random float between 5 and 10
 num = random.uniform(5, 10)
 print("Random float between 5 and 10:", num)
+
+o/p:Random float between 5 and 10: 6.344379634241552
 
