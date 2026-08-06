128712a207a9dcc861093416ba00491a0a468924
Nidhi-VP <nidhipoojari2207@gmail.com>
2026-08-06 12:38:54 +0530

    While-loop

---
+++ day11.py
@@ -0,0 +1,17 @@
+Use a while loop to print even numbers from 2 to 20.
+
+# Print even numbers from 2 to 20 using a while loop
+num = 2
+while num <= 20:
+    print(num)
+    num += 2
+
+o/p:4
+6
+8
+10
+12
+14
+16
+18
+20
