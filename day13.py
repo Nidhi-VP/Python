bb438a947a06cc5cc91f43d13829522ed55a284c
Nidhi-VP <nidhipoojari2207@gmail.com>
2026-08-07 21:26:14 +0530

    Largest of three numbers.

---
+++ day13.py
@@ -0,0 +1,14 @@
+Write a program to find the largest of three numbers.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Largest number is:", max(a, b, c))
+
+
+o/p:
+Enter first number: 55
+Enter second number: 98
+Enter third number: 48
+Largest number is: 98
